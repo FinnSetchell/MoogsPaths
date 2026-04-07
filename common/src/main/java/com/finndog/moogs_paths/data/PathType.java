3bbba6e8dc2673c94654332ba1cd1f8489a09479
finnsetchell <finn@setchell.org>
2026-04-07 20:17:31 +0200

    added PathType and ScaleSettings

---
+++ common/src/main/java/com/finndog/moogs_paths/data/PathType.java
@@ -0,0 +1,55 @@
+package com.finndog.moogs_paths.data;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.List;
+
+public record PathType(
+    List<WeightedBlock> surfaceBlocks,
+    List<WeightedBlock> edgeBlocks,
+    ResourceLocation fillBlock,
+    WidthRange width,
+    float curviness,
+    SlopeHandling slopeHandling,
+    FadeSettings fade
+) {
+    public record WeightedBlock(ResourceLocation block, int weight) {
+        public static final Codec<WeightedBlock> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            ResourceLocation.CODEC.fieldOf("block").forGetter(WeightedBlock::block),
+            Codec.INT.fieldOf("weight").forGetter(WeightedBlock::weight)
+        ).apply(instance, WeightedBlock::new));
+    }
+
+    public record WidthRange(int min, int max) {
+        public static final Codec<WidthRange> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            Codec.INT.fieldOf("min").forGetter(WidthRange::min),
+            Codec.INT.fieldOf("max").forGetter(WidthRange::max)
+        ).apply(instance, WidthRange::new));
+    }
+
+    public record SlopeHandling(int cutTolerance, int fillTolerance) {
+        public static final Codec<SlopeHandling> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            Codec.INT.fieldOf("cut_tolerance").forGetter(SlopeHandling::cutTolerance),
+            Codec.INT.fieldOf("fill_tolerance").forGetter(SlopeHandling::fillTolerance)
+        ).apply(instance, SlopeHandling::new));
+    }
+
+    public record FadeSettings(int startBlocks, int endBlocks) {
+        public static final Codec<FadeSettings> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            Codec.INT.fieldOf("start_blocks").forGetter(FadeSettings::startBlocks),
+            Codec.INT.fieldOf("end_blocks").forGetter(FadeSettings::endBlocks)
+        ).apply(instance, FadeSettings::new));
+    }
+
+    public static final Codec<PathType> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+        Codec.list(WeightedBlock.CODEC).fieldOf("surface_blocks").forGetter(PathType::surfaceBlocks),
+        Codec.list(WeightedBlock.CODEC).fieldOf("edge_blocks").forGetter(PathType::edgeBlocks),
+        ResourceLocation.CODEC.fieldOf("fill_block").forGetter(PathType::fillBlock),
+        WidthRange.CODEC.fieldOf("width").forGetter(PathType::width),
+        Codec.FLOAT.fieldOf("curviness").forGetter(PathType::curviness),
+        SlopeHandling.CODEC.fieldOf("slope_handling").forGetter(PathType::slopeHandling),
+        FadeSettings.CODEC.fieldOf("fade").forGetter(PathType::fade)
+    ).apply(instance, PathType::new));
+}
